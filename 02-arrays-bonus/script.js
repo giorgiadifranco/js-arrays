8bddd77f0dfc10463c2f4377a69a0af768a91e15
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-02 16:19:16 +0200

    add_splice

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -18,6 +18,24 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+/*const longNames = teachers.filter(myfunction) 
+
+function myFunction(array) {
+
+  return array.filter( >=5
+  
+}
+
+
+console.log(longNames);*/
+
+/*const numbers = [45, 4, 9, 16, 25];
+const over18 = numbers.filter(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 18;
+}*/
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(5)
+console.log(teachers);
